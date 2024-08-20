d0c2efecdd4cf54384fbd55c143d7108e5e739fe
manuelpacheco108 <manuelpm108@gmail.com>
2024-08-19 19:57:47 -0500

    Implementacion en cambios de los archivos babel.config.js, index.js y ademas de subir la aplicación principal con los cambios realizados

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import HomeScreen from "./src/screens/HomeScreen";
 import HamburguerNavigation from "./src/navigation/HamburguerNavigation";
-import { NavigationContainer } from "@react-navigation/native";
-
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <HamburguerNavigation/>
-    </NavigationContainer>
-  
-);
+    <HamburguerNavigation/>
+  );
 }
 
 export default App;
