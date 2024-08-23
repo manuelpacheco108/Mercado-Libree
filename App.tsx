c3335892e7e52e701e170f1d8e361433f4ed6b6f
manuelpacheco108 <manuelpm108@gmail.com>
2024-08-22 23:20:34 -0500

    Se actualio momentaneamente para terminos de definicion de pantallas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from "./src/screens/HomeScreen";
+import PaymentScreen from './src/screens/Payments';
 
 const App = () => {
     return (
         <NavigationContainer>
-            <HomeScreen />
+            <HomeScreen/>
         </NavigationContainer>
     );
 };
