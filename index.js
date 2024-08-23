f324c988f3b7f5361501669cb7ebc6ebbbe09e26
manuelpacheco108 <manuelpm108@gmail.com>
2024-08-23 18:27:36 -0500

    Archivos de configuracion creados por react. index.js y stylesHomeScreeen.js

--- index.js
+++ index.js
@@ -2,17 +2,15 @@
  * @format
  */
 import './gesture-handler';
-import {AppRegistry} from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 export default function Main() {
-    return (
-      <PaperProvider>
-        <App />
-      </PaperProvider>
-    );
-  }
-  
-AppRegistry.registerComponent(appName, () => Main);
+  return (
+    <App />
+
+  );
+}
+
+AppRegistry.registerComponent(appName, () => App);
